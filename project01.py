03817250d42f8e300474948a7e1d89955d1c5a9b
Nisha Baidya <163021505+nisha2418baidya@users.noreply.github.com>
2024-10-19 01:51:37 +0530

    Add files via upload

---
+++ project01.py
@@ -0,0 +1,218 @@
+#for the first student
+student1 = {}
+
+student1["Name"] = input("Enter the first student name: ")
+student1["maths"] = int(input("Enter the maths marks : "))
+student1["phy"] = int(input("Enter the phy marks: "))
+student1["bio"] = int(input("Enter the bio marks: "))
+
+print(student1)
+
+
+
+average = (student1["maths"] + student1["phy"] + student1["bio"] )/3
+
+print(average)
+
+
+if (student1["maths"]>student1["phy"] and student1["maths"]>student1["bio"]) :
+    print("highest number is maths:", student1["maths"])
+
+elif(student1["phy"]>student1["maths"] and student1["phy"]>student1["bio"]) :
+    print("highest number is phy:", student1["phy"])  
+
+elif(student1["bio"]>student1["maths"] and student1["bio"]>student1["phy"]) :
+    print("highest number is bio:", student1["bio"]) 
+
+
+
+
+if (student1["maths"]<student1["phy"] and student1["maths"]<student1["bio"]) :
+    print("lowest number is maths:", student1["maths"])
+
+elif(student1["phy"]<student1["maths"] and student1["phy"]<student1["bio"]) :
+    print("lowest number is phy:", student1["phy"])  
+
+elif(student1["bio"]<student1["maths"] and student1["bio"]<student1["phy"]) :
+    print("lowest number is bio:", student1["bio"]) 
+
+name = input("Enter the name :")
+if student1["Name"] == name :
+    del student1["Name"]
+    print(student1)
+else:
+ print("Entered name is invalid")
+
+
+maths = int(input("Enter the proper maths marks :"))
+if student1["maths"] == maths:
+    del student1["maths"]
+    print(student1)
+else:
+ print("Entered marks is invalid")
+
+
+phy = int(input("Enter the proper phy marks :"))
+if student1["phy"] == phy:
+    del student1["phy"]
+    print(student1)
+else:
+ print("Entered marks is invalid")
+
+
+bio = int(input("Enter the proper bio marks :"))
+if student1["bio"] == bio:
+    del student1["bio"]
+    print(student1)
+else:
+ print("Entered marks is invalid")
+
+
+
+
+
+#for the 2nd student
+
+student2 = {}
+
+student2["Name"] = input("Enter the first student name: ")
+student2["maths"] = int(input("Enter the maths marks : "))
+student2["phy"] = int(input("Enter the phy marks: "))
+student2["bio"] = int(input("Enter the bio marks: "))
+
+print(student1)
+
+
+
+average = (student2["maths"] + student2["phy"] + student2["bio"] )/3
+
+print(average)
+
+
+if (student2["maths"]>student2["phy"] and student2["maths"]>student2["bio"]) :
+    print("highest number is maths:", student2["maths"])
+
+elif(student2["phy"]>student2["maths"] and student2["phy"]>student2["bio"]) :
+    print("highest number is phy:", student2["phy"])  
+
+elif(student2["bio"]>student2["maths"] and student2["bio"]>student2["phy"]) :
+    print("highest number is bio:", student2["bio"]) 
+
+
+
+
+if (student2["maths"]<student2["phy"] and student2["maths"]<student2["bio"]) :
+    print("lowest number is maths:", student2["maths"])
+
+elif(student2["phy"]<student2["maths"] and student2["phy"]<student2["bio"]) :
+    print("lowest number is phy:", student2["phy"])  
+
+elif(student2["bio"]<student2["maths"] and student2["bio"]<student2["phy"]) :
+    print("lowest number is bio:", student2["bio"])
+
+
+name = input("Enter the name :")
+if student2["Name"] == name :
+    del student2["Name"]
+    print(student2)
+else:
+ print("Entered name is invalid")
+
+
+maths = int(input("Enter the proper maths marks :"))
+if student2["maths"] == maths:
+    del student2["maths"]
+    print(student2)
+else:
+ print("Entered marks is invalid")
+
+
+phy = int(input("Enter the proper phy marks :"))
+if student2["phy"] == phy:
+    del student2["phy"]
+    print(student2)
+else:
+ print("Entered marks is invalid")
+
+
+bio = int(input("Enter the proper bio marks :"))
+if student2["bio"] == bio:
+    del student2["bio"]
+    print(student2)
+else:
+ print("Entered marks is invalid")
+
+
+    
+
+
+#for the third student
+
+student3 = {}
+
+student3["Name"] = input("Enter the first student name: ")
+student3["maths"] = int(input("Enter the maths marks : "))
+student3["phy"] = int(input("Enter the phy marks: "))
+student3["bio"] = int(input("Enter the bio marks: "))
+
+print(student3)
+
+
+
+average = (student3["maths"] + student3["phy"] + student3["bio"] )/3
+
+print(average)
+
+
+if (student3["maths"]>student3["phy"] and student3["maths"]>student3["bio"]) :
+    print("highest number is maths:", student3["maths"])
+
+elif(student3["phy"]>student3["maths"] and student3["phy"]>student3["bio"]) :
+    print("highest number is phy:", student3["phy"])  
+
+elif(student3["bio"]>student3["maths"] and student3["bio"]>student3["phy"]) :
+    print("highest number is bio:", student3["bio"]) 
+
+
+
+
+if (student3["maths"]<student3["phy"] and student3["maths"]<student3["bio"]) :
+    print("lowest number is maths:", student3["maths"])
+
+elif(student3["phy"]<student3["maths"] and student3["phy"]<student3["bio"]) :
+    print("lowest number is phy:", student3["phy"])  
+
+elif(student3["bio"]<student3["maths"] and student3["bio"]<student3["phy"]) :
+    print("lowest number is bio:", student3["bio"]) 
+
+
+name = input("Enter the name :")
+if student3["Name"] == name :
+    del student3["Name"]
+    print(student3)
+else:
+ print("Entered name is invalid")
+
+
+maths = int(input("Enter the proper maths marks :"))
+if student3["maths"] == maths:
+    del student3["maths"]
+    print(student3)
+else:
+ print("Entered marks is invalid")
+
+
+phy = int(input("Enter the proper phy marks :"))
+if student3["phy"] == phy:
+    del student3["phy"]
+    print(student3)
+else:
+ print("Entered marks is invalid")
+
+
+bio = int(input("Enter the proper bio marks :"))
+if student3["bio"] == bio:
+    del student3["bio"]
+    print(student3)
+else:
+ print("Entered marks is invalid")
